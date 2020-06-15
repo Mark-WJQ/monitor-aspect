099c381bfd8216819edc0573d2071f2df55e078e
wangjianqiang24 <wangjianqiang24@jd.com>
2020-06-15 15:23:32 +0800

    结果转换

--- eco-monitor/eco-monitor-component-spring-boot-starter/src/test/java/com/jd/jr/eco/component/monitor/starter/execute/TestAopTest.java
+++ eco-monitor/eco-monitor-component-spring-boot-starter/src/test/java/com/jd/jr/eco/component/monitor/starter/execute/TestAopTest.java
@@ -1,15 +1,12 @@
 package com.jd.jr.eco.component.monitor.starter.execute;
 
-import ch.qos.logback.classic.Level;
-import ch.qos.logback.classic.Logger;
-import ch.qos.logback.classic.LoggerContext;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
 
 /**
  * @author wangjianqiang24
@@ -20,15 +17,11 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 public class TestAopTest {
 
-
     @Autowired
     private TestAop testAop;
 
     @Test
     public void monitor() {
-        LoggerContext context = (LoggerContext) LoggerFactory.getILoggerFactory();
-        Logger logger = context.getLogger("root");
-        logger.setLevel(Level.DEBUG);
         testAop.monitor(null);
     }
 }
